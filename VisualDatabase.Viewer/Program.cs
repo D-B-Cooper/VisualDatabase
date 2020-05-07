b0212eea9bb7a1bdefe7b82f277c54b4c0cf4425
Ian <mahaffy.2@wright.edu>
2020-05-07 16:46:00 -0400

    Initialize Project

---
+++ VisualDatabase.Viewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualDatabase.Viewer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ VisualDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualDatabase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
